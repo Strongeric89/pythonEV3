4092f7156e12eed83d14ab9b29d94483be13ece7
Strongeric89 <ericstrong89@gmail.com>
2016-12-27 19:34:58 +0000

    followline with automatic threshold

--- threshold.py
+++ threshold.py
@@ -8,6 +8,7 @@ ts = ev3.TouchSensor()
 assert ts.connected
 cs = ev3.ColorSensor()
 assert cs.connected
+button = ev3.Button()
 
 
 # main follow line logic
@@ -15,11 +16,14 @@ def followLine(threshold):
     flag  = 0
     speed = 100
     while(flag == 0):
+        if button.any():
+            flag = 1
         while(cs.value() < threshold):
             mA.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
 
         while (cs.value() > threshold):
             mB.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
+
 
 def getValue(option):
     if(option == 1):
@@ -54,3 +58,4 @@ print('threshold =' + str(threshold))
 ev3.Sound.speak('The threshold value is ' + str(threshold)).wait()
 
 followLine(threshold)
+ev3.Sound.speak('program end').wait()
