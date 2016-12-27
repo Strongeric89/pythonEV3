8858315f97605b332b31a776f981ee5206dfb35d
Strongeric89 <ericstrong89@gmail.com>
2016-12-27 16:01:31 +0000

    Ev3 Python - move4 until touch sensor is touched - added leds

--- move4.py
+++ move4.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
+
+def led():
+    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
 #configuring motors
 ts = ev3.TouchSensor()
 mA = ev3.LargeMotor('outA')
@@ -13,6 +16,7 @@ while(True):
     mA.run_forever(speed_sp=-400)
     mB.run_forever(speed_sp=400)
     ev3.Sound.speak(count).wait()
+    led()
     count +=1
 #if count reaches 10 stop the motors
 mA.stop()
