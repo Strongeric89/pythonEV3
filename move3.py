aad05c625f69648b299c20fb94fa3db6b83a5fd3
Strongeric89 <ericstrong89@gmail.com>
2016-12-27 15:39:49 +0000

    Ev3 Python - move3 - move in a loop . circle

--- move3.py
+++ move3.py
@@ -4,8 +4,10 @@ import ev3dev.ev3 as ev3
 mA = ev3.LargeMotor('outA')
 mB = ev3.LargeMotor('outB')
 
-
+count = int(0)
 while(True):
+    mA.run_forever(speed_sp=400)
+    mB.run_forever(speed_sp=-400)
+    ev3.Sound.speak(count).wait()
+    count +=1
 
-	mA.run_forever(speed_sp=400)
-	mB.run_forever(speed_sp=-400)
