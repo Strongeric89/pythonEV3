bfda9cf56948525e61b4b03dcdf3fac1aaf3957d
Strongeric89 <ericstrong89@gmail.com>
2016-12-27 16:22:31 +0000

    Ev3 Python - buttons - touch buttons shows the state edited

--- Buttons.py
+++ Buttons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
-speed = 100
+speed = 600
 def led():
     ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
 
@@ -17,20 +17,20 @@ def left(state):
 def right(state):  # neater use of 'if' follows:
     print('Right button pressed' if state else 'Right button released')
     mB.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
-    ev3.Sound.speak('moving to the left').wait()
+    ev3.Sound.speak('moving to the right').wait()
 
 
 def up(state):
     print('Up button pressed' if state else 'Up button released')
-    mA.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
-    mB.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
+    mA.run_to_rel_pos(position_sp="180", speed_sp=-speed, stop_action="hold")
+    mB.run_to_rel_pos(position_sp="180", speed_sp=-speed, stop_action="hold")
 
 
 
 def down(state):
     print('Down button pressed' if state else 'Down button released')
-    mA.run_to_rel_pos(position_sp="180", speed_sp=-speed, stop_action="hold")
-    mB.run_to_rel_pos(position_sp="180", speed_sp=-speed, stop_action="hold")
+    mA.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
+    mB.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
 
 
 
