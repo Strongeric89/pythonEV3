8dfcf5bc3800eea0ac67d225585d6d7f8528d681
Strongeric89 <ericstrong89@gmail.com>
2016-12-27 19:29:57 +0000

    followline with automatic threshold

--- threshold.py
+++ threshold.py
@@ -9,35 +9,48 @@ assert ts.connected
 cs = ev3.ColorSensor()
 assert cs.connected
 
-class threshold(object):
-    def getValue(option):
-        if(option == 1):
-            ev3.Sound.speak('press touch sensor when on bright color').wait()
-            while (ts.value() == 0):
-                #empty loop
-                filer = 1
 
-            value = cs.value()
-            print('the bright value is ' + str(value))
-            ev3.Sound.speak('the bright value is ' + str(value)).wait()
-            return value
-        else :
-            ev3.Sound.speak('press touch sensor when on dark color').wait()
-            while (ts.value() ==0):
-                #empty loop
-                filer = 1
-            value = cs.value()
-            print('the dark value is ' + str(value))
-            ev3.Sound.speak('the dark value is ' + str(value)).wait()
-            return value
-        def main():
-        #main
-        ev3.Sound.speak('get brigth value').wait()
-        bright = float(getValue(1))
-        ev3.Sound.speak('get dark value').wait()
-        dark = float(getValue(2))
+# main follow line logic
+def followLine(threshold):
+    flag  = 0
+    speed = 20
+    while(flag == 0):
+        while(cs.value() < threshold):
+            mA.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
 
-        #get threshold value
-        threshold = float((bright + dark) / 2)
-        print('threshold =' + str(threshold))
-        ev3.Sound.speak('The threshold value is ' + str(threshold)).wait()
+        while (cs.value() > threshold):
+            mB.run_to_rel_pos(position_sp="180", speed_sp=speed, stop_action="hold")
+
+def getValue(option):
+    if(option == 1):
+        ev3.Sound.speak('press touch sensor when on bright color').wait()
+        while (ts.value() == 0):
+            #empty loop
+            filer = 1
+
+        value = cs.value()
+        print('the bright value is ' + str(value))
+        ev3.Sound.speak('the bright value is ' + str(value)).wait()
+        return value
+    else :
+        ev3.Sound.speak('press touch sensor when on dark color').wait()
+        while (ts.value() ==0):
+            #empty loop
+            filer = 1
+        value = cs.value()
+        print('the dark value is ' + str(value))
+        ev3.Sound.speak('the dark value is ' + str(value)).wait()
+        return value
+
+#main
+ev3.Sound.speak('get brigth value').wait()
+bright = float(getValue(1))
+ev3.Sound.speak('get dark value').wait()
+dark = float(getValue(2))
+
+#get threshold value
+threshold = float((bright + dark) / 2)
+print('threshold =' + str(threshold))
+ev3.Sound.speak('The threshold value is ' + str(threshold)).wait()
+
+followLine(threshold)
