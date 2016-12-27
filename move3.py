b5d61df26e7b6ceae2d173eeca7527d1b05fb894
Strongeric89 <ericstrong89@gmail.com>
2016-12-27 15:42:07 +0000

    Ev3 Python - move3 - move in a loop . circle

--- move3.py
+++ move3.py
@@ -6,8 +6,13 @@ mB = ev3.LargeMotor('outB')
 
 count = int(0)
 while(True):
+    if(count == 10):
+        break
     mA.run_forever(speed_sp=400)
     mB.run_forever(speed_sp=-400)
     ev3.Sound.speak(count).wait()
     count +=1
+#if count reaches 10 stop the motors
+mA.stop()
+mB.stop()
 
